e57a39d0f5590bedcba323e5519db66945189d2c
ManuelBasso <manuel.basso.89@gmail.com>
2024-02-05 17:46:50 +0100

    updated Seller class

--- src/main/java/com/develhope/spring/entities/typeOfUsers/Seller.java
+++ src/main/java/com/develhope/spring/entities/typeOfUsers/Seller.java
@@ -1,5 +1,33 @@
 package com.develhope.spring.entities.typeOfUsers;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Table(name = "sellers")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Seller {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
 }
