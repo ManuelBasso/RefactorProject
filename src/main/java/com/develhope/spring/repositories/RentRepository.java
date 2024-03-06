34a4115f44c951685372d4a7ece2373a2da57095
Scalisi Danilo <147306723+Scalisi-Danilo@users.noreply.github.com>
2024-03-06 19:57:40 +0100

    Delete src/main/java/com/develhope/spring/repositories directory

--- src/main/java/com/develhope/spring/repositories/RentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.develhope.spring.repositories;
-
-import com.develhope.spring.entities.StatusOfVehicle.Rent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RentRepository extends JpaRepository<Rent, Long> {
-}
