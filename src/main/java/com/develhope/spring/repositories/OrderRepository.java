cafb578fea507b9a4218718ed581aa922161fad6
TheManufacturer <antonio.mario.demarco@gmail.com>
2024-03-06 19:58:10 +0100

    Delete Package repository

--- src/main/java/com/develhope/spring/repositories/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.develhope.spring.repositories;
-
-import com.develhope.spring.entities.StatusOfVehicle.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
