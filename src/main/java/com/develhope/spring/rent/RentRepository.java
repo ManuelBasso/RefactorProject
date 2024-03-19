b97108ff0f0211e984498c7e98732bfb1077abae
TheManufacturer <antonio.mario.demarco@gmail.com>
2024-03-19 15:30:26 +0100

    Fixed Query in RentRepository

--- src/main/java/com/develhope/spring/rent/RentRepository.java
+++ src/main/java/com/develhope/spring/rent/RentRepository.java
@@ -11,9 +11,8 @@ import java.time.OffsetDateTime;
 public interface RentRepository extends JpaRepository<RentInfo, Long> {
 
     void deleteByCustomer_Id(Long userId);
-/*
+
     //OK for RENT
-    @Query(value = "SELECT SUM(r.total_cost) FROM Rents r WHERE r.startDate BETWEEN :startDate AND :endDate AND r.seller_id = :sellerId")
+    @Query(value = "SELECT SUM(r.total_cost) FROM Rents r WHERE r.start_date BETWEEN :startDate AND :endDate AND seller_id = :sellerId", nativeQuery = true)
     Double cashOutForSellerInDateRent(@Param("startDate") OffsetDateTime startDate, @Param("endDate") OffsetDateTime endDate, @Param("sellerId") Long sellerId);
-*/
 }
